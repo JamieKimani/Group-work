80af25be487eaae865846566b5a6df2636bf057c
JamieKimani <153290082+JamieKimani@users.noreply.github.com>
2026-03-10 20:50:32 +0300

    second commit

--- Shop/urls.py
+++ Shop/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('login/', views.login, name='login'),
-]
+    path('register/', views.register, name='register'),
+    path('employee_login/', views.employee_login, name='employee_login'),
+    path('checkout/', views.checkout, name='checkout'),
+]   
--- Shop/views.py
+++ Shop/views.py
@@ -8,12 +8,10 @@ def about(request):
     return render(request, 'about.html')
 
 def login(request):
-<<<<<<< HEAD
     return render(request, 'login.html')
+def employee_login(request):
+    return render(request, 'employee_login.html')
 def register(request):
     return render(request, 'register.html')
-def collection(request):
-    return render(request, 'collection.html')
-=======
-    return render(request, 'login.html')
->>>>>>> 407b0d9569c3306b04be13ba3f58bee13dec5303
+def checkout(request):
+    return render(request, 'checkout.html')
